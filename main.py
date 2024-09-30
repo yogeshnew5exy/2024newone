09e39602da8d84480ea3286c0266236665297f4c
yogeshnew5exy <149184955+yogeshnew5exy@users.noreply.github.com>
2024-09-30 10:57:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,19 +27,19 @@ bot = Client("bot",
              api_hash= "4ccc4865eec4d8fde7530e71948b3424")
 
 
-@bot.on_message(filters.command(["start"]))
+@app.on_message(filters.command(["start"]))
 async def account_login(bot: Client, m: Message):
     editable = await m.reply_text(f"Hello [{m.from_user.first_name}](tg://user?id={m.from_user.id})\nPress /covid")
 
 
-@bot.on_message(filters.command("stop"))
+@app.on_message(filters.command("stop"))
 async def restart_handler(_, m):
     await m.reply_text("**STOPPED**🚦", True)
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 
 
-@bot.on_message(filters.command(["covid"]))
+@app.on_message(filters.command(["covid"]))
 async def account_login(bot: Client, m: Message):
     editable = await m.reply_text('Send TXT file for download')
     input: Message = await bot.listen(editable.chat.id)
