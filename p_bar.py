43403c7021b57bcdecc4a24a0878e833683c0d07
yogeshnew5exy <149184955+yogeshnew5exy@users.noreply.github.com>
2025-01-22 14:55:57 +0530

    Update p_bar.py

--- p_bar.py
+++ p_bar.py
@@ -117,11 +117,10 @@ async def progress_bar(current, total, reply, start):
             bar_length = 11
             completed_length = int(current * bar_length / total)
             remaining_length = bar_length - completed_length
-            progress_bar = "▰" * completed_length + "▱" * remaining_length
+            progress_bar = "😘" * completed_length + "💃" * remaining_length
             
             try:
-                  await reply.edit(f'<b>\n ╭─⌯══⟰ 𝐔𝐩𝐥𝐨𝐝𝐢𝐧𝐠 ⟰══⌯──★ \n├⚡ {progress_bar}|﹝{perc}﹞ \n├🚀 Speed » {sp} \n├📟 Processed » {cur}\n├🧲 Size - ETA » {tot} - {eta} \n╰─══😘 🇾 🇴 🇬 🇪 🇸 🇭 🥰══─★\n</b>') 
-
+                await reply.edit(f'`╭──⌈🌸💕𝑢℘ɩ٥ǻⅆꪱทℊ-💕🌸⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰────⌈🦋⁂༄𝙔őɠ𝕖ᔆ𝘩༄⁂🦋⌋────╯`\n') 
+         #       await reply.edit(f'`╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰─⌈  𝘽𝙤𝙩 𝙈𝙖𝙙𝙚 𝙗𝙮 🇨‌ 🇴‌ 🇻 🇮 🇩 ⌋─╯`\n') 
             except FloodWait as e:
                 time.sleep(e.x)
-
